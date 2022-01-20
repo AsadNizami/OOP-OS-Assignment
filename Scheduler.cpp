7850b9943ff1d66a7aa6b46303ba652d09898064
Asad <asadnizami123@gmail.com>
2022-01-20 15:52:57 +0530

    RR Completed

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -23,6 +23,13 @@ struct record{
     double curr_time;
     int pid;
     string status;
+    int order;
+};
+
+struct compare_order{
+    bool operator()(struct record &r1, struct record &r2){
+        return r1.order > r2.order;
+    }
 };
 
 bool comp_at(Process &p1, Process &p2){
@@ -141,7 +148,7 @@ public:
     }
 
     void helper_process(vector<Process> &proc, vector<double> first_t){
-        // STRF
+        // STRF and RR
         forn(i, proc.size()){
             calc(proc[i]);
             proc[i].res_time = first_t[i+1] - proc[i].arr_time;
@@ -192,6 +199,7 @@ public:
 
                 ready_q.push(pr.back());
                 pr.pop_back();
+                
                 if (ready_q.top().burst_time < ele.burst_time){
                     insert(rec, ele, "Preempted", curr_time);
 
@@ -202,6 +210,7 @@ public:
                     if(first_t[ele.pid] == -1)  first_t[ele.pid] = curr_time;
                     ready_q.pop();
                 }
+                
             }
 
             else{
@@ -228,14 +237,111 @@ public:
         display_process(proc);
     }
 
-    void RR(Process &pr, double tq){}
+    void RR(vector<Process> &proc, double tq=0.5){
+        if (tq < 0.01){
+            cout << "Invalid Time Quantum";
+            return;
+        }
+        vector<Process> pr_copy = proc;
+        vector<record> pr(pr_copy.size());
+        sort(pr_copy.begin(), pr_copy.end(), comp_at);
+        reverse(pr_copy.begin(), pr_copy.end());
+
+        forn(i, pr_copy.size()){
+            pr[i].pid = pr_copy[i].pid;
+        }
+
+        priority_queue<record, vector<record>, compare_order> ready_q;
+
+        vector<double> first_t(pr.size()+1);
+        vector<double> burst_cpy(pr.size()+1);
+        vector<record> rec;
+        double curr_time = 0;
+        bool flag = true;
+        int ord = 0, processed = 0, size = pr.size();
+        struct record ele;
+
+        forn(i, pr.size()){
+            first_t[i+1] = -1;
+            burst_cpy[i+1] = proc[i].burst_time;
+        }
+
+        ready_q.push(pr.back());
+        pr.pop_back();
+
+        while (processed != size){
+
+            if (ready_q.size() == 0 and pr.size() != 0 and flag){
+                pr.back().order = ord++;
+                ready_q.push(pr.back());
+                pr.pop_back();
+                continue;
+            }
+            else if (flag){
+                ele = ready_q.top();
+                ready_q.pop();
+                if (curr_time < proc[ele.pid-1].arr_time){
+                    curr_time = proc[ele.pid-1].arr_time;
+                    insert(rec, proc[ele.pid-1], "Arrived", curr_time);
+                }                
+                insert(rec, proc[ele.pid-1], "Running", curr_time);
+                if(first_t[ele.pid] == -1)  first_t[ele.pid] = curr_time;
+                flag = false;
+            }
+
+            else if (pr.size() != 0 and proc[pr.back().pid-1].arr_time <= min(proc[ele.pid-1].burst_time, tq) + curr_time){
+                pr.back().order = ord++;
+                insert(rec, proc[pr.back().pid-1], "Arrived", proc[pr.back().pid-1].arr_time);
+
+                ready_q.push(pr.back());
+                pr.pop_back();                
+            }
+
+            else if(proc[ele.pid-1].burst_time > tq){
+                proc[ele.pid-1].burst_time -= tq;
+                curr_time += tq;
+                insert(rec, proc[ele.pid-1], "Stopped", curr_time);
+                ele.order = ord++;
+                ready_q.push(ele);
+
+                ele = ready_q.top();
+                ready_q.pop();
+                insert(rec, proc[ele.pid-1], "Running", curr_time);
+                if(first_t[ele.pid] == -1)  first_t[ele.pid] = curr_time;
+            }
+            else{
+                curr_time += proc[ele.pid-1].burst_time;
+                proc[ele.pid-1].comp_time = curr_time; 
+                insert(rec, proc[ele.pid-1], "Exit", curr_time);
+                proc[ele.pid-1].comp_time = curr_time;
+                processed++;
+
+                if (ready_q.size() != 0){
+                    ele = ready_q.top();
+                    ready_q.pop();
+                    insert(rec, proc[ele.pid-1], "Running", curr_time);
+                    if(first_t[ele.pid] == -1)  first_t[ele.pid] = curr_time;
+                }
+                else{
+                    flag = true;
+                }
+            }
+        }
+        display_status(rec);
+        
+        forn(i, size){
+            proc[i].burst_time = burst_cpy[i+1];
+        }
+        helper_process(proc, first_t);
+        display_process(proc);
+    }
 };
 
 
-int main(){
-    Scheduler sch;
-    Process_Creator prc(5);
-
-    sch.SRTF(prc.pr);
-    forn(i, 5)  prc.pr[i].print_info();
-}
+// int main(){
+//     Scheduler sch;
+//     Process_Creator prc(5);
+//     // forn(i, 5)  prc.pr[i].print_info();
+//     sch.RR(prc.pr);
+//     forn(i, 5)  prc.pr[i].print_info();
+// }
